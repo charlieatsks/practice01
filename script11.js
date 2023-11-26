3c94aad6215d88e33c730e85d523ca03f8a5f68c
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2023-11-26 21:03:03 +0530

    Update script11.js

--- script11.js
+++ script11.js
@@ -11,7 +11,10 @@ let page = 1;
 async function searchImages(){
     keyword = searchBox.Value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
-
+    // page above for content
+    // keyword above is for search keyword
+    // accessKey is to excess the details
+    
     const response = await fetch(url);
     const data = await response.json();
 
