b35d26d9fcb98112508edff27936bc309838d3c5
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2023-11-28 22:54:16 +0530

    Add files via upload

---
+++ script16.js
@@ -0,0 +1,30 @@
+let availableKeywords = [
+    'HTML',
+    'CSS',
+    'Web design tutorials',
+    '',
+    '',
+    '',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availableKeywords.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+}
+
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>"
+}
