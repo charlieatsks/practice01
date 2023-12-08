891ee7846925cf7dd191d4611a6c35d2ca5963fe
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2023-12-08 22:32:19 +0530

    Add files via upload

--- script24.js
+++ script24.js
@@ -1,7 +1,65 @@
-let [seconds, minutes, hours] = [0,0,0];
+let [mili,seconds, minutes, hours] = [0,0,0,0];
 let displayTime = document.getElementById("displayTime");
 let timer = null;
 
+// stop watch now counts 1/100 seconds or miliseconds
+function stopwatch(){
+    mili++;
+    if(mili == 100){
+        mili = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerHTML = h +":"+ m +":"+ s +":"+ mili;
+}//  
+
+function watchStart(){
+    if(timer!== null)
+    {
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,10);
+}
+
+function watchStop(){
+    clearInterval(timer);
+}
+function watchReset(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00 F";
+}
+
+/*
+
+function stopwatch(){
+    mili++;
+    if(mili == 100){
+        mili = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+// for stop watch of 1 secconds
 function stopwatch(){
     seconds++;
     if(seconds == 60){
@@ -18,7 +76,7 @@ function stopwatch(){
     let s = seconds < 10 ? "0" + seconds : seconds;
 
     displayTime.innerHTML = h +":"+ m +":"+ s;
-}
+}  
 
 function watchStart(){
     if(timer!== null)
@@ -27,13 +85,6 @@ function watchStart(){
     }
     timer = setInterval(stopwatch,1000);
 }
+// 1000 is time duration above
 
-function watchStop(){
-    clearInterval(timer);
-}
-function watchReset(){
-    clearInterval(timer);
-    [seconds, minutes, hours] = [0,0,0];
-    displayTime.innerHTML = "00:00:00";
-}
-
+*/
