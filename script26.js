e61ad819ed1e4e41818e0063a5b373cb61069e06
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2023-12-15 19:30:38 +0530

    Add files via upload

--- script26.js
+++ script26.js
@@ -3,11 +3,25 @@ const addButton = document.querySelector(".add-button");
 const todosHtml = document.querySelector(".todos");
 const emptyImage = document.querySelector(".em-images");
 let todosJson = JSON.parse(localStorage.getItem("todos")) || [];
+const deleteAllButton = document.querySelector(".delete-all");
+const filters = document.querySelectorAll(".filter");
+let filter = '';
+
+showTodos();
 
 function getTodoHtml(todo, index) {
+    if (filter && filter != todo.status) {
+        return '';
+    }
     let checked = todo.status == "completed" ? "checked" : "";
     return /* HTML */`
         <li class="todo">
+            <label>
+
+
+
+
+
     `;
 }
 
@@ -27,6 +41,63 @@ function addTodo(todo) {
     localStorage.setItem("todos", JSON.stringify(todosJson));
     showTodos();
 }
+
+input.addEventListener("keyup", e => {
+    let todo = input.value.trim();
+    if (!todo || e.key != "Enter") {
+        return;
+    }
+    addTodo(todo);
+});
+
+addButton.addEventListener("click", () => {
+    let todo = input.value.trim();
+    if (!todo) {
+        return;
+    }
+    addTodo(todo);
+});
+
+function updateStatus(todo) {
+    let todoName = todo.parentElement.lastElementClild;
+    if (todo.checked) {
+        todoName.classList.add("checked");
+        todosJson[todo.id].status = "completed";
+    } else {
+        todoName.classList.remove("checked");
+        todosJson[todo.id].status = "pending";
+    }
+    localStorage.setItem("todos", JSON.stringify(todosJson));
+}
+
+function remove(todo) {
+    const index = todo.dataset.index;
+    todosJson.splice(index, 1);
+    showTodos();
+    localStorage.setItem("todos", JSON.stringify(todosJson));
+}
+
+filters.forEach(function (el) {
+    el.addEventListener("click", (e) => {
+        if (el.classList.contains('active')){
+            el.classList.remove('active');
+            filter = '';
+        } else {
+            filters.forEach(tag => tag.classList.remove('active'));
+            el.classList.add('active');
+            filter = e.target.dataset.filter;
+        }
+        showTodos();
+    });
+});
+
+deleteAllButton.addEventListener("click", () => {
+    todosJson = [];
+    localStorage.setItem("todos", JSON.stringify(todosJson));
+    showTodos();
+});
+
+
 
 
 
