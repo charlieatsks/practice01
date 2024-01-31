0a8fa453c1d6c8c7b161ff30986933c3de05bb37
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2024-01-31 23:33:05 +0530

    Add files via upload

--- Jan/script05.js
+++ Jan/script05.js
@@ -23,18 +23,32 @@ const transactions = [
 ];
 
 
-/*
-const transactions = [];
-const list = document.getElementById("transactionList");*/
 
 const list = document.getElementById("transactionList");
-const form = document.getElementById("transactoinForm");
 const status = document.getElementById("status");
+const form = document.getElementById("transactionForm");
 const balance = document.getElementById("balance");
 const income = document.getElementById("income");
 const expense = document.getElementById("expense");
 
 form.addEventListener("submit", addTransaction);
+
+function updateTotal() {
+    const incomeTotal = transactions
+    .filter((trx) => trx.type === "income")
+    .reduce((total, trx) => total + trx.amount, 0);
+
+    const expenseTotal = transactions
+    .filter((trx) => trx.type === "expense")
+    .reduce((total, trx) => total + trx.amount, 0);
+    
+    const balanceTotal = incomeTotal - expenseTotal;
+
+    balance.textContent = formatter.format(balanceTotal).substring(1);
+    income.textContent = formatter.format(incomeTotal);
+    expense.textContent = formatter.format(expenseTotal * -1);
+}
+
 
 const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -44,18 +58,24 @@ const formatter = new Intl.NumberFormat("en-US", {
 
 
 function renderList() {
-    // list.innerHTML = "<li>item</li>";
+    //list.innerHTML = "<li>item</li>";
     list.innerHTML = "";
 
-    if (transactions.length === 0) {
-        status.textContent = "No transactions. ";
+    if(transactions.length === 0) {
+        status.textContent = "No transactions.";
         return;
     } else {
         status.textContent = "";
     }
 
-    transactions.forEach(({id, name, amount, date, type} ) => {
+//{id, name, amount, date, type} 
+//{ name, date, amount }
+    transactions.forEach(({ id, name, date, amount, type }) => {
+
+        const sign = "income" === type ? 1 : -1;
         const li = document.createElement("li");
+
+        //li.innerHTML = transaction.amount;
 
         li.innerHTML = `
             <div class="name">
@@ -64,14 +84,14 @@ function renderList() {
             </div>
 
             <div class="amount ${type}" >
-                <span>${formatter.format(amount)}</span>
+            <span>${formatter.format(amount * sign)}</span>
             </div>
-
             <div class="action">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" onclick="deleteTransaction(${id})">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
             </div>
+            
         `;
 
         list.appendChild(li);
@@ -79,38 +99,41 @@ function renderList() {
 }
 
 renderList();
-/*
-const transactions = JSON.parse(localStorage.getItem("transtions")) || [];
-*/
-/*
+updateTotal();
+
 function deleteTransaction(id) {
-    alert("delete item");
+    //alert("delete item");
     const index = transactions.findIndex((trx) => trx.id === id);
     transactions.splice(index, 1);
 
+    updateTotal();
     renderList();
 }
 
-/*
 
 function addTransaction(e) {
+    //alert(formData.get("name"));
     e.preventDefault();
 
     const formData = new FormData(this);
-/*
+
     transactions.push({
         id: transactions.length + 1,
         name: formData.get("name"),
         amount: parseFloat(formData.get("amount")),
-        date: new Date(formData.get("date")),/*
-        type: "on" === formData.get("type") ? "income" : "expense",*/
-   // });
-/*
+        date: new Date(formData.get("date")),
+        type: "on" === formData.get("type") ? "income" : "expense",
+    });
+
     this.reset();
 
-    renderList();/*
     updateTotal();
-    saveTransactions();
-    
-    alert(formData.get("name"));*/
-//}
+    renderList();
+
+}
+
+
+/*
+
+
+*/
