4139e7a820c6216d334333df92cf772efc018451
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2024-04-30 00:00:12 +0530

    Add files via upload

--- script01.js
+++ script01.js
@@ -1,55 +1,256 @@
-let n = 16;
+let products = [];
+let cart = [];
 
-for ( let i = n ; i >= 1; i=i-3 ) {
-    let str = "* ";
-    let space = '  ';
-    console.log(space.repeat(n-i) + str.repeat(i));
 
+//- selectors
+
+const selectors = {
+    products: document.querySelector('.products'),
+    cartBtn: document.querySelector('.cart-btn'),
+    cartQty: document.querySelector('.cart-qty'),
+    cartClose: document.querySelector('.cart-close'),
+    cart: document.querySelector('.cart'),
+    cartOverlay: document.querySelector('.cart-overlay'),
+    cartClear: document.querySelector('.cart-clear'),
+    cartBody: document.querySelector('.cart-body'),
+    cartTotal: document.querySelector('.cart-total'),
+};
+
+//- event listeners
+
+const setupListeners = () => {
+    document.addEventListener('DOMContentLoaded', initStore);
+
+    // product event 
+    selectors.products.addEventListener('click', addToCart);
+    
+    // cart events
+    selectors.cartBtn.addEventListener('click', showCart);
+    selectors.cartOverlay.addEventListener('click', hideCart);
+    selectors.cartClose.addEventListener('click', hideCart);
+    selectors.cartBody.addEventListener('click', updateCart);
+    selectors.cartClear.addEventListener('click', clearCart);
+};
+
+
+//- event handlers
+
+const initStore = () => {
+    loadCart();
+    loadProducts("https://fakestoreapi.com/products").then(renderProducts)
+    .finally(renderCart);
+};
+
+const showCart = () => {
+    selectors.cart.classList.add('show');
+    selectors.cartOverlay.classList.add('show');
 }
 
-
-let m = 5;
-for ( let i = m; i >= 1; i--) {
-    let str = '* ';
-    console.log(str.repeat(i));
+const hideCart = () => {
+    selectors.cart.classList.remove('show');
+    selectors.cartOverlay.classList.remove('show');
 }
 
+const clearCart = () => {
+    cart = [];
+    saveCart();
+    renderCart();
+    renderProducts();
+    setTimeout(hideCart, 500);
+};
 
-let a = 6;
-for ( let i = 1; i <= a; i++ ) {
-    let str = '* ';
-    console.log(str.repeat(i));
-}
+const addToCart = (e) => {
+    if (e.target.hasAttribute("data-id")) {
+        const id = parseInt(e.target.dataset.id);
+        const inCart = cart.find((x) => x.id === id);
 
-let b = 5;
-for ( let i = 1; i <= b ; i++ ) {
-    let str = '* ';
-    let space = '  ';
-    console.log(space.repeat((b-i)) + str.repeat(i));
-}
-
-
-/*
-const items = ['sks', 'shlok', 'sunny', 'abhi', 'charlie', 'phoyam', 'sk', 'cd', 'dani'];
-
-for (let i = items.length - 1 ; i >= 0; i-- ) {
-
-    console.log(`${i+1} . ${items[i] }`);
-}
-*//*
-
-for ( let outer = 0; outer < 2 ; outer += 1 ) {
-    for ( let inner = 0 ; inner < 3 ; inner += 1 ) {
-        for ( let innerinner = 0 ; innerinner < 3 ; innerinner += 1 ) {
-            console.log (`${outer} - ${inner} - ${innerinner} `);
+        if (inCart) {
+            alert("Item is already in cart.");
+            return;
         }
-    }
-}
 
-for ( let outer = 0; outer < 2 ; outer += 1 ) {
-    for ( let inner = 0 ; inner < 3 ; inner += 1 ) {
-            console.log (`${outer} - ${inner} `);
+        cart.push({ id, qty: 1 });
+        saveCart();
+        renderProducts();
+        renderCart();
+        showCart();
     }
-}
+};
 
-*/
+const removeFromCart = (id) => {
+    cart = cart.filter(x => x.id !== id);
+
+    // if the last item is remove, close the cart
+    cart.length === 0 && setTimeout(hideCart, 500);
+
+    renderProducts();
+};
+
+const incQty = (id) => {
+    const item = cart.find((x) => x.id === id);
+    if(!item) return ;
+
+    item.qty++;
+};
+
+const decQty = (id) => {
+    const item = cart.find((x) => x.id === id);
+    if(!item) return ;
+
+    item.qty--;
+
+    if(item.qty === 0) removeFromCart(id)
+};
+
+const updateCart = (e) => {
+    if(e.target.hasAttribute('data-btn')) {
+        const cartItem = e.target.closest(".cart-item");
+        const id = parseInt(cartItem.dataset.id);
+        const btn = e.target.dataset.btn;
+
+        btn === "incr" && incQty(id);
+        btn === "decr" && decQty(id);
+
+        saveCart();
+        renderCart();
+    }
+};
+
+
+const saveCart = () => {
+    localStorage.setItem("online-store" , JSON.stringify(cart));
+};
+
+const loadCart = () => {
+    cart = JSON.parse(localStorage.getItem("online-store")) || [];
+};
+
+/*  const initStore = () => {
+        fetch("https://fakestoreapi.com/products/1")
+            .then((res) => res.json())
+            .then((json) => console.log(json));
+};       */
+
+
+//- render functions
+
+const renderCart = () => {
+
+    // show cart qty in navbar
+    selectors.cartQty.textContent = cart.reduce((sum, item) => {
+        return sum + item.qty;
+    }, 0);
+
+    // show cart total 
+    selectors.cartTotal.textContent = calculateTotal().format();   
+
+    // show empty cart
+    if (cart.length === 0) {
+        selectors.cartBody.innerHTML = `<div class="cart-empty">Your cart is Empty.</div> `;
+        return;
+    }
+
+    // show cart items
+    selectors.cartBody.innerHTML = cart.map(({ id, qty }) => {
+        
+        // get product info of each cart item
+        const product = products.find((x) => x.id === id);
+
+        const { title, image, price } = product;
+
+        const amount = price * qty;
+
+        return `
+        <div class="cart-item" data-id="${id}">
+                <img src="${image}" alt="${title}">
+                <div class="cart-item-detail">
+                    <h3>${title}</h3>
+                    <h5>${price.format()}</h5>
+                    <div class="cart-item-amount">
+                        <i class="bi bi-dash-lg" data-btn="decr"></i>
+                        <span class="qty">${qty}</span>
+                        <i class="bi bi-plus-lg" data-btn="incr"></i>
+
+                        <span class="cart-item-price">
+                        ${amount.format()}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        `;
+    }).join("");
+};
+
+
+const renderProducts = () => {
+    selectors.products.innerHTML = products.map(product => {
+        const { id, title, image, price } = product;
+        
+        // check if product is already in cart
+        const inCart = cart.find((x) => x.id === id);
+
+        // make the add to cart button disabled if already in cart
+        const disabled = inCart ? "disabled" : "";
+
+        // change the text if already in cart
+        const text = inCart ? "Added in Cart" : "Add to Cart";
+        
+        return `
+        <div class="product">
+                    <img src="${image}" alt="${title}">
+                    <h3>${title}</h3>
+                    <h5>${price.format()}</h5>
+                    <button ${disabled} data-id=${id}>${text}</button>
+                </div>
+        `;
+    }) .join("");
+};
+
+
+//- api functions
+
+const loadProducts = async (apiURL) => {
+    try {
+        const response = await fetch(apiURL);
+        if (!response.ok) {
+            throw new Error(`http error! status=${response.status}`);
+        }
+        products = await response.json();
+        console.log(products);
+    } catch(error) {
+        console.error("fetch error:", error);
+    }
+};
+
+
+//- helper functions
+
+const calculateTotal = () => {
+    return cart 
+    .map(({ id, qty }) => {
+        const  { price } = products.find((x) => x.id === id);
+        
+        return qty * price;
+    })
+    .reduce((sum, number) => {
+        return sum + number;
+    }, 0);
+};
+
+
+Number.prototype.format = function () {
+    return this.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",/* Use INR for Rupees Symbol here */
+    });
+};
+
+
+//- initialize 
+
+setupListeners();
+
+
+
+// fakestoreapi website 
+
